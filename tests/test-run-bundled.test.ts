08a1670bcf88b6694ff1541fd50b61c74c2db883
SkS <i@shutovks.ru>
2026-02-17 13:57:11 +0300

    test: US-001 - use dist imports in bundled workflow tests

--- tests/test-run-bundled.test.ts
+++ tests/test-run-bundled.test.ts
@@ -1,8 +1,8 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
-import { resolveBundledWorkflowDir, resolveBundledWorkflowsDir } from "../src/installer/paths.js";
-import { listBundledWorkflows } from "../src/installer/workflow-fetch.js";
-import { loadWorkflowSpec } from "../src/installer/workflow-spec.js";
+import { resolveBundledWorkflowDir, resolveBundledWorkflowsDir } from "../dist/installer/paths.js";
+import { listBundledWorkflows } from "../dist/installer/workflow-fetch.js";
+import { loadWorkflowSpec } from "../dist/installer/workflow-spec.js";
 import path from "node:path";
 
 describe("bundled workflows", () => {
